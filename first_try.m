e447b7e09e4f351173bff79c2144eb4647cc7ab9
b07901064 <peiyi.lin@tum.de>
2022-07-06 14:09:40 +0200

    edit first_try.m, seperate on time axis also did short ch subtraction

--- first_try.m
+++ first_try.m
@@ -3,8 +3,10 @@ close all;
 clc;
 % load('/Users/bettylin2727/Downloads/AtlasViewer/Group/DataTree/AcquiredData/Snirf/Examples/neuro_run01.nirs', '-mat');
 % load('/Users/bettylin2727/Downloads/AtlasViewer/Group/DataTree/AcquiredData/Snirf/Examples/neuro_run01.snirf');
-[snirf_saved, snirf_loaded, nirs] = snirf_load_save('/Users/bettylin2727/Downloads/AtlasViewer/Group/DataTree/AcquiredData/Snirf/Examples/neuro_run01.nirs');
-
+[snirf_saved, snirf_loaded, nirs] = snirf_load_save('/Users/bettylin2727/Downloads/sub_liao.nirs');
+nReps= 8;
+time_perStim = 45;
+sampling_freq = 50;
 %% Remove bad channels according SCI
 nb_channels = size(snirf_loaded.data.measurementList,2)/2;
 sci = get_SCI(snirf_loaded.data, nb_channels);
@@ -36,7 +38,11 @@ ylabel('delta optical density');
 
 %% Motion Artifact correction
 % mlActMan, mlActAuto ??
-dod_MA_corrected = hmrR_MotionCorrectWavelet(dod, cell(length(dod),1),cell(length(dod),1) ,1.5);
+% dod_MA_corrected = hmrR_MotionCorrectWavelet(dod, cell(length(dod),1),cell(length(dod),1) ,1.5);
+
+
+% PCA remove one component: one source motion artifact
+dod_MA_corrected = hmrR_MotionCorrectPCA(dod, [],[],[],[], 2);
 
 figure();
 plot(time, dod_MA_corrected.dataTimeSeries(:,1),'r'); hold on;
@@ -52,5 +58,92 @@ data_bp = hmrR_BandpassFilt(dod_MA_corrected, 0.5, 0.01);
 %% OD to Conc (apply MBLL)
 dc = hmrR_OD2Conc( data_bp, snirf_loaded.probe, 1 );
 
+%{
+14 channels
+3 concentration
+8 reps
+4 volumns
+%}
+
 %% Reduce extracerebral component with short channels
+% ROI 1
+for i = 0:2
+    dc.dataTimeSeries(:, 1*3-i) = dc.dataTimeSeries(:, 1*3-i) - dc.dataTimeSeries(:, 13*3-i);
+    dc.dataTimeSeries(:, 2*3-i) = dc.dataTimeSeries(:, 2*3-i) - dc.dataTimeSeries(:, 13*3-i);
+    dc.dataTimeSeries(:, 5*3-i) = dc.dataTimeSeries(:, 5*3-i) - dc.dataTimeSeries(:, 13*3-i);
+
+    % ROI3
+    dc.dataTimeSeries(:, 3*3-i) = dc.dataTimeSeries(:, 3*3-i) - dc.dataTimeSeries(:, 13*3-i);
+    dc.dataTimeSeries(:, 4*3-i) = dc.dataTimeSeries(:, 4*3-i) - dc.dataTimeSeries(:, 13*3-i);
+    dc.dataTimeSeries(:, 8*3-i) = dc.dataTimeSeries(:, 8*3-i) - dc.dataTimeSeries(:, 13*3-i);
+
+    % ROI5
+    dc.dataTimeSeries(:, 14*3-i) = dc.dataTimeSeries(:, 14*3-i) - dc.dataTimeSeries(:, 13*3-i);
+    dc.dataTimeSeries(:, 6*3-i) = dc.dataTimeSeries(:, 6*3-i) - dc.dataTimeSeries(:, 11*3-i);
+    dc.dataTimeSeries(:, 9*3-i) = dc.dataTimeSeries(:, 9*3-i) - dc.dataTimeSeries(:, 11*3-i);
+
+    % ROI7
+    dc.dataTimeSeries(:, 7*3-i) = dc.dataTimeSeries(:, 7*3-i) - dc.dataTimeSeries(:, 11*3-i);
+    dc.dataTimeSeries(:, 10*3-i) = dc.dataTimeSeries(:, 10*3-i) - dc.dataTimeSeries(:, 11*3-i);
+    dc.dataTimeSeries(:, 12*3-i) = dc.dataTimeSeries(:, 12*3-i) - dc.dataTimeSeries(:, 11*3-i);
+
+end
+%% starting points
+
+% starting time, unit is sec
+start_idx = 1 + 50*  min([snirf_loaded.stim(1,1).data(1,1), snirf_loaded.stim(1,2).data(1,1), snirf_loaded.stim(1,3).data(1,1), snirf_loaded.stim(1,4).data(1,1)]);
+% starting points for 4 stimuli
+start_idx_A = 1.+ 50* snirf_loaded.stim(1,1).data(:,1);
+start_idx_B = 1.+ 50* snirf_loaded.stim(1,2).data(:,1);
+start_idx_C = 1.+ 50* snirf_loaded.stim(1,3).data(:,1);
+start_idx_D = 1.+ 50* snirf_loaded.stim(1,4).data(:,1);
+
+%% First set
+A_1 = dc.dataTimeSeries(start_idx_A(1):start_idx_A(1)+ 45*50 -1 ,:);
+B_1 = dc.dataTimeSeries(start_idx_B(1):start_idx_B(1)+ 45*50 -1 ,:);
+C_1 = dc.dataTimeSeries(start_idx_C(1):start_idx_C(1)+ 45*50 -1 ,:);
+D_1 = dc.dataTimeSeries(start_idx_D(1):start_idx_D(1)+ 45*50 -1 ,:);
+%  Plot 14 channels
+plot_14Ch(A_1, B_1, C_1, D_1);
+
+
+%% Timeseries seperation
+% devide vertically
+
+cell_A = cell(1,8);
+cell_B = cell(1,8);
+cell_C = cell(1,8);
+cell_D = cell(1,8);
+
+sum_A = zeros(45*50, 42);
+sum_B = zeros(45*50, 42);
+sum_C = zeros(45*50, 42);
+sum_D = zeros(45*50, 42);
+
+
+for i = 1: nReps
+    
+    cell_A{1,i} = dc.dataTimeSeries(start_idx_A(i):start_idx_A(i)+ 45*50 -1 ,:);
+    cell_B{1,i} = dc.dataTimeSeries(start_idx_B(i):start_idx_B(i)+ 45*50 -1 ,:);
+    cell_C{1,i} = dc.dataTimeSeries(start_idx_C(i):start_idx_C(i)+ 45*50 -1 ,:);
+    cell_D{1,i} = dc.dataTimeSeries(start_idx_D(i):start_idx_D(i)+ 45*50 -1 ,:);
+    
+    sum_A = sum_A + cell_A{1,i};
+    sum_B = sum_B + cell_B{1,i};
+    sum_C = sum_C + cell_C{1,i};
+    sum_D = sum_D + cell_D{1,i};
+    
+end
+
+
+avg_A = sum_A./nReps;
+avg_B = sum_B./nReps;
+avg_C = sum_C./nReps;
+avg_D = sum_D./nReps;
+
+%% Plot 14 channels
+plot_14Ch(avg_A, avg_B, avg_C, avg_D);
+
+
+%% 
 printf('');
